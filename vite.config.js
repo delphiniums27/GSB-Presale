47a7717078995c7d2a9b4eaf10664eb4fa595f78
delphiniums27 <herdelphiniums@gmail.com>
2025-03-02 00:29:55 +0400

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "https://globalstable.io/buy",
-  server: { port: 3000 },
-});
